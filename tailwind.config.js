15f7eacdd2bc7ed0a34c36acb069d18d31376d63
samanehkarimi <samaneh.karimi.78nn@gmail.com>
2023-08-30 13:07:19 +0330

    sChanging the thermometer with another thermometer

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,11 @@ module.exports = {
         'mobile': '360px'
         // => @media (min-width: 640px) { ...
       },
-      top:{
-        '1/5': '100px',
+      spacing:{
+        '42': '166px',
+      },
+      boxShadow: {
+        '3xl': '4px 8px 16px 0 rgba(0, 0, 0, 0.1)',
       }
   },
   plugins: [],
