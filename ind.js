ff59c5d27e9ac2a71a697d12e87e323183c15c87
samanehkarimi <samaneh.karimi.78nn@gmail.com>
2023-09-09 12:16:38 +0330

    s Completion of thermometer with hichart and position of buttons

--- ind.js
+++ ind.js
@@ -1,71 +1,105 @@
-$(function () {//from w w w .j  av a  2  s. com
-    var gaugeOptions = {
-        chart: {
-            type: 'solidgauge'
-        },
-        title: null,
-        pane: {
-            size: '90%',
-            startAngle: -180,
-            endAngle: 90,
-            background: {
-                backgroundColor: '#EEE',
-                innerRadius: '95%',
-                outerRadius: '100%',
-                shape: 'arc'
-            }
-        },
-        tooltip: {
-            enabled: false
-        },
-        // the value axis
-        yAxis: {
-            stops: [
-                [0, '#000088'],
-                [29 / 90, '#000088'],
-                [30 / 90, '#5555ff'],
-                [41 / 90, '#5555ff'],
-                [42 / 90, '#00ff00'],
-                [54 / 90, '#00ff00'],
-                [55 / 90, '#ff8c00'],
-                [59 / 90, '#ff8c00'],
-                [60 / 90, '#ff0000']
-            ],
-            lineWidth: 0,
-            minorTickInterval: 0,
-            tickPixelInterval: 50,
-            tickWidth: 1,
-            labels: {
-                enabled: true,
-                distance: 10
-            }
-        },
-        plotOptions: {
-            solidgauge: {
-                innerRadius: '95%',
-                dataLabels: {
-                    y: 5,
-                    borderWidth: 0,
-                    useHTML: true
-                }
+
+// fetch('http://example.com/temperature')
+// .then(response => response.json())
+// .then(data => {
+
+
+var gaugeOptions = {
+    chart: {
+        type: 'solidgauge',
+        backgroundColor:'#374151',
+        
+    },
+
+    title: null,
+
+    pane: {
+        size: '87%',
+        startAngle: -180,
+        endAngle: 90,
+        background: {
+            backgroundColor: '#EEE',
+            innerRadius: '95%',
+            outerRadius: '100%',
+            shape: 'arc'
+        }
+    },
+
+    exporting: {
+        enabled: false
+    },
+
+    tooltip: {
+        enabled: false
+    },
+
+    // the value axis
+    yAxis: {
+        stops: [
+            [0, '#000088'],
+            [29 / 90, '#000088'],
+            [30 / 90, '#5555ff'],
+            [41 / 90, '#5555ff'],
+            [42 / 90, '#00ff00'],
+            [54 / 90, '#00ff00'],
+            [55 / 90, '#ff8c00'],
+            [59 / 90, '#ff8c00'],
+            [60 / 90, '#ff0000']
+        ],
+
+        lineWidth: 0,
+        minorTickInterval: 0,
+        tickPixelInterval: 50,
+        tickWidth: 1,
+        labels: {
+            style:{
+                color:'white'
+            },
+            enabled: true,
+            distance: 10
+        }
+    },
+
+    plotOptions: {
+        solidgauge: {
+            innerRadius: '95%',
+            dataLabels: {
+                y: 5,
+                borderWidth: 0,
+                useHTML: true
             }
         }
-    };
+    }
+};
+var chartSpeed = Highcharts.chart('container', Highcharts.merge(gaugeOptions, {
+    yAxis: {  
+        min: -30,
+        max: 60,
+        title: {
+            text: 'thermometer',
+            style:{
+                color:'white'
+            },
+        }
+    },
 
-    $('#temp001').highcharts(Highcharts.merge(gaugeOptions, {
-        yAxis: {
-            min: -30,
-            max: 60
+    credits: {
+        enabled: false
+    },
+    series: [{
+        name: 'inTemp',
+        data: [25], //data.temperature
+        dataLabels: {
+            format:
+                '<div style="text-align:center">' +
+                '<span style="font-size:25px">{y} &deg;C</span><br/>' +
+                '</div>'
         },
-        credits: {
-            enabled: false
-        },
-        series: [{
-            name: 'inTemp',
-            data: [20], /////// Temp Value //////////
-            dataLabels: {
-                format: '<div style="text-align:center"><span style="font-size:25px;color:#000' + ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black') + '">{y} &deg;C</span><br/>' + '<span style="font-size:12px;color:silver">abcde</span></div>'
-            }
-        }]
-    }));
-});
+        tooltip: {
+            valueSuffix: '{y} &deg;C'
+        }
+    }]
+
+}));
+// });
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,12 @@ module.exports = {
       },
       spacing: {
         '32': '32px',
+        '34':'135px',
+        '70':'287'
       },
       inset: {
         '4/4': '85%',
+        '1/5': '23%',
       },
       boxShadow: {
         '3xl': '4px 8px 16px 0 rgba(0, 0, 0, 0.1)',
